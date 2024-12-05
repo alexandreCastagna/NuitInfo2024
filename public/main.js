c9d13ae31b67741e3e6c77e87e1c8bf992df14ce
CASTAGNA Alexandre <CASTAGNA@lan.sio.lyceefulbert.fr>
2024-12-06 00:58:42 +0100

    CA FONCTIONNE !!!!!!!!!!!

--- public/main.js
+++ public/main.js
@@ -20,23 +20,63 @@
 // }
 
 
-function j_ai_reson(num_reponse){
-    docQuiFonctionne = document.getElementById('mes_infos');
+// function j_ai_reson(num_reponse){
+//     docQuiFonctionne = document.getElementById('mes_infos');
 
-    var tem = document.getElementById('mes_infos').maTematique;
-    var num = docQuiFonctionne.monnumero;
-    docQuiFonctionne.monnumero = num-1;
-    var data = 1
-    fetch('/question_api').then((data)=>{console.log(data.json());
-     return data.json()}).then((datadeux)=>{data= datadeux})
-    console.log(data);
-    let question_actuellel;
-    data.forEach(element => {
-        if (element['numero'] == num && tem == element['thème'] ){
-            question_actuellel = element;
-        }
-        
-    });
+//     var tem = document.getElementById('mes_infos').maTematique;
+//     var num = docQuiFonctionne.monnumero;
+//     docQuiFonctionne.monnumero = num-1;
+//     var data = 1
+//     fetch('/question_api').then((data)=>{console.log(data.json());
+//     return data.json()}).then((data)=>{
+//     console.log(data);
+//     let question_actuellel;
+//     data.forEach(element => {
+//         if (element['numero'] == num && tem == element['thème'] ){
+//             question_actuellel = element;
+//         }
+//     } 
+//     );
 
-    return (question_actuelle['numReponse'] == num_reponse)
+//     return (question_actuelle['numReponse'] == num_reponse)
+// })
+// }
+function l(params) {console.log(params);}
+
+function j_ai_reson(num_reponse) {
+    // Get the necessary DOM elements
+    // docQuiFonctionne = document.getElementById('mes_infos');
+    var tem =  document.getElementById('mes_infos').getAttribute('matemateique');
+    var num =  document.getElementById('mes_infos').getAttribute('monnumero');
+    document.getElementById('mes_infos').monnumero = num - 1;
+     l( document.getElementById('mes_infos'))
+    console.log(tem)
+    l(num)
+    // Fetch data from the API
+    fetch('/question_api')
+        .then((response) => response.json())  // Read the response once
+        .then((data) => {
+            console.log(data);  // Now you have the parsed data
+
+            // Initialize variable to store the matched question
+            let question_actuellel;
+
+            // Loop through the data to find the matching question
+            data.forEach(element => {
+                console.log(element["numero"]);
+                if (element['numero'] == num && tem == element['thème']) {
+                    console.log("AAAAA")
+                    question_actuellel = element;
+                }
+            });
+
+            // Return whether the question matches the num_reponse
+            return question_actuellel['numReponse'] == num_reponse;
+        })
+        .then((result) => {
+            console.log(result);  // This will log `true` or `false` based on the condition
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);  // Catch any errors
+        });
 }
